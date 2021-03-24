bf3959733f08dd0e9e82360e55562aa720d561c6
jzakotnik <jure@zakotnik.de>
2021-03-24 15:13:06 +0100

    filter implemented

--- src/Layout/Filter.js
+++ src/Layout/Filter.js
@@ -1,88 +1,50 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import ListItemText from "@material-ui/core/ListItemText";
-import Select from "@material-ui/core/Select";
-import Checkbox from "@material-ui/core/Checkbox";
-import Chip from "@material-ui/core/Chip";
+
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
 
 import { useHistory } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-    maxWidth: 300,
+const useStyles = makeStyles({
+  root: {
+    width: 600,
   },
-  chips: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-  chip: {
-    margin: 2,
-  },
-  noLabel: {
-    marginTop: theme.spacing(3),
-  },
-}));
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+});
 
 const Filter = (props) => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [year, setYear] = React.useState([]);
+  const [value, setValue] = useState([1980, 2010]);
+  const [marks, setMarks] = useState([]);
+  const years = props.years;
 
-  const handleChange = (event) => {
-    setYear(event.target.value);
-    props.filterYears(event.target.value);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    props.filterYears(newValue);
+    //console.log(newValue);
   };
 
-  const handleChangeMultiple = (event) => {
-    const { options } = event.target;
-    const value = [];
-    for (let i = 0, l = options.length; i < l; i += 1) {
-      if (options[i].selected) {
-        value.push(options[i].value);
-      }
-    }
-    setYear(value);
-    props.filterYears(value);
-  };
+  function valuetext(value) {
+    return `${value}`;
+  }
 
   return (
-    <div>
-      <h3>Filter</h3>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-mutiple-name-label">Jahrgang</InputLabel>
-        <Select
-          labelId="demo-mutiple-name-label"
-          id="demo-mutiple-name"
-          multiple
-          value={year}
-          onChange={handleChange}
-          input={<Input />}
-          MenuProps={MenuProps}
-        >
-          {props.years.map((v) => (
-            <MenuItem key={v} value={v}>
-              {v}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+    <div className={classes.root}>
+      <Typography id="range-slider" gutterBottom>
+        Jahrgänge
+      </Typography>
+      <Slider
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+        step={10}
+        marks
+        min={1950}
+        max={2025}
+        step={1}
+        getAriaValueText={valuetext}
+      />
     </div>
   );
 };
